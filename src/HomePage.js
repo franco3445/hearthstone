42bdd27156977c594230d3c116e86d85f9b96b5e
gFranco <gfranco@velocitymsc.com>
2024-01-08 16:45:40 -0600

    add rest of lookup buttons

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,15 @@
 import axios from "axios";
+import * as React from 'react';
 import './HomePage.css';
 
 function HomePage() {
 
-    const getCurrentPatchInformation = async function () {
+    const [inputValue, setInputValue] = React.useState('')
+
+    const reachHearthStoneEndpoint = async function (endpoint) {
         const options = {
             method: 'GET',
-            url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/info',
+            url: `https://omgvamp-hearthstone-v1.p.rapidapi.com/${endpoint}`,
             headers: {
                 'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
                 'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
@@ -20,16 +23,26 @@ function HomePage() {
             console.error(error);
         }
     }
-    getCurrentPatchInformation();
-      return (
+
+    return (
         <div className="HomePage">
-          <header className="HomePage-header">
-            <h1>HearthStone Data Finder</h1>
-              <p>Enter card information</p>
-              <input/>
-          </header>
+            <header className="HomePage-header">
+                <h1>HearthStone Data Finder</h1>
+                <p>Enter card information</p>
+                <input value={inputValue} onInput={e => setInputValue(e.target.value)}/>
+                <button onClick={e => reachHearthStoneEndpoint(e.target.value)} value={'info'}>Get Patch Notes</button>
+                <button onClick={e => reachHearthStoneEndpoint(e.target.value)} value={'cards'}>Get All Cards</button>
+                <button onClick={e => reachHearthStoneEndpoint(e.target.value)} value={`cards/search/${inputValue}`}>Search Card</button>
+                <button onClick={e => reachHearthStoneEndpoint(e.target.value)} value={`cards/classes/${inputValue}`}>Search by Class</button>
+                <button onClick={e => reachHearthStoneEndpoint(e.target.value)} value={`cards/races/${inputValue}`}>Search by Race</button>
+                <button onClick={e => reachHearthStoneEndpoint(e.target.value)} value={`cards/sets/${inputValue}`}>Search by Set</button>
+                <button onClick={e => reachHearthStoneEndpoint(e.target.value)} value={`cards/qualities/${inputValue}`}>Search by Qualities</button>
+                <button onClick={e => reachHearthStoneEndpoint(e.target.value)} value={`cards/factions/${inputValue}`}>Search by Faction</button>
+                <button onClick={e => reachHearthStoneEndpoint(e.target.value)} value={`cards/types/${inputValue}`}>Search by Type</button>
+                <button onClick={e => reachHearthStoneEndpoint(e.target.value)} value={`cardbacks`}>Get CardBacks</button>
+            </header>
         </div>
-      );
+    );
 }
 
 export default HomePage;
