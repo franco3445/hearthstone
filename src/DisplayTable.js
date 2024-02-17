a2142759915690e8c3dacd9239cb1b1f19b92a79
gFranco <gfranco@velocitymsc.com>
2024-02-17 13:33:04 -0600

    move table to own component

---
+++ src/DisplayTable.js
@@ -0,0 +1,233 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+
+import {
+    useTheme,
+    Box,
+    Button,
+    IconButton,
+    MenuItem,
+    Paper,
+    Select,
+    SelectChangeEvent,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableFooter,
+    TableHead,
+    TablePagination,
+    TableRow,
+    TextField,
+    Typography,
+} from '@mui/material';
+
+function TablePaginationActions(props) {
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onPageChange } = props;
+
+    const handleFirstPageButtonClick = (event) => {
+        onPageChange(event, 0);
+    };
+
+    const handleBackButtonClick = (event) => {
+        onPageChange(event, page - 1);
+    };
+
+    const handleNextButtonClick = (event) => {
+        onPageChange(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = (event) => {
+        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+
+    return (
+        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+            <IconButton
+                aria-label="first page"
+                disabled={page === 0}
+                onClick={handleFirstPageButtonClick}
+            >
+                {theme.direction === 'rtl'
+                    ? <LastPageIcon />
+                    : <FirstPageIcon />
+                }
+            </IconButton>
+            <IconButton
+                aria-label="previous page"
+                disabled={page === 0}
+                onClick={handleBackButtonClick}
+            >
+                {theme.direction === 'rtl'
+                    ? <KeyboardArrowRight />
+                    : <KeyboardArrowLeft />
+                }
+            </IconButton>
+            <IconButton
+                aria-label="next page"
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                onClick={handleNextButtonClick}
+            >
+                {theme.direction === 'rtl'
+                    ? <KeyboardArrowLeft />
+                    : <KeyboardArrowRight />
+                }
+            </IconButton>
+            <IconButton
+                aria-label="last page"
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                onClick={handleLastPageButtonClick}
+            >
+                {theme.direction === 'rtl'
+                    ? <FirstPageIcon />
+                    : <LastPageIcon />
+                }
+            </IconButton>
+        </Box>
+    );
+}
+
+TablePaginationActions.propTypes = {
+    count: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+};
+
+export function DisplayTable(props) {
+    const [apiResults, setApiResults] = React.useState([]);
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    
+    const {
+        results
+    } = props;
+
+    React.useEffect(() => {
+        setApiResults(results);
+    }, [results]);
+
+    const tableHeaderLabels = [
+        'Name',
+        'Card Set',
+        'Type',
+        'Class',
+        'Cost',
+    ]
+    
+    const emptyRows = page > 0
+        ? Math.max(0, (1 + page) * rowsPerPage - apiResults.length)
+        : 0;
+    
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+    
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+    
+    const tableHeader = (
+        <TableHead>
+            <TableRow>
+                {tableHeaderLabels.map(label => {
+                    return (
+                        <TableCell align="center">
+                            {label}
+                        </TableCell>
+                    )
+                })}
+            </TableRow>
+        </TableHead>
+    );
+    
+    const tableFooter = (
+        <TableFooter>
+            <TableRow>
+                <TablePagination
+                    colSpan={3}
+                    count={apiResults.length}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                    page={page}
+                    rowsPerPage={rowsPerPage}
+                    rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                    ActionsComponent={TablePaginationActions}
+                    SelectProps={{
+                        inputProps: {
+                            'aria-label': 'rows per page',
+                        },
+                        native: true,
+                    }}
+                />
+            </TableRow>
+        </TableFooter>
+    );
+    
+    const emptyTableResults = (
+        <TableRow style={{ height: 53 }}>
+            <TableCell align="center" colSpan={5}  >
+                Nothing to report...
+            </TableCell>
+        </TableRow>
+    );
+    const tableWithResults = (rowsPerPage > 0
+            ? apiResults.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : apiResults
+    ).map((row) => (
+        <TableRow
+            key={row.dbfId}
+            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+        >
+            <TableCell
+                align="center"
+                component="th"
+                scope="row"
+            >
+                {row.name}
+            </TableCell>
+            <TableCell align="center">
+                {row.cardSet}
+            </TableCell>
+            <TableCell align="center">
+                {row.type}</TableCell>
+            <TableCell align="center">
+                {row.playerClass}
+            </TableCell>
+            <TableCell align="center">
+                {row.cost}
+            </TableCell>
+        </TableRow>
+    ));
+    
+    return (
+        <TableContainer component={Paper} sx={{maxHeight: 500}}>
+            <Table
+                aria-label="simple table"
+                size="small"
+                sx={{ minWidth: 650 }}
+                stickyHeader
+            >
+                {tableHeader}
+                <TableBody>
+                    {apiResults.length === 0
+                        ? emptyTableResults
+                        : tableWithResults}
+                    {emptyRows > 0 && (
+                        <TableRow style={{ height: 53 * emptyRows }}>
+                            <TableCell colSpan={6} />
+                        </TableRow>
+                    )}
+                </TableBody>
+                {tableFooter}
+            </Table>
+        </TableContainer>
+    );
+}
--- src/HomePage.js
+++ src/HomePage.js
@@ -30,84 +30,17 @@ import {
 
 import './HomePage.css';
 
-function TablePaginationActions(props) {
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onPageChange } = props;
-
-    const handleFirstPageButtonClick = (event) => {
-        onPageChange(event, 0);
-    };
-
-    const handleBackButtonClick = (event) => {
-        onPageChange(event, page - 1);
-    };
-
-    const handleNextButtonClick = (event) => {
-        onPageChange(event, page + 1);
-    };
-
-    const handleLastPageButtonClick = (event) => {
-        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
-
-    return (
-        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-            <IconButton
-                aria-label="first page"
-                disabled={page === 0}
-                onClick={handleFirstPageButtonClick}
-            >
-                {theme.direction === 'rtl'
-                    ? <LastPageIcon />
-                    : <FirstPageIcon />
-                }
-            </IconButton>
-            <IconButton
-                aria-label="previous page"
-                disabled={page === 0}
-                onClick={handleBackButtonClick}
-            >
-                {theme.direction === 'rtl'
-                    ? <KeyboardArrowRight />
-                    : <KeyboardArrowLeft />
-                }
-            </IconButton>
-            <IconButton
-                aria-label="next page"
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                onClick={handleNextButtonClick}
-            >
-                {theme.direction === 'rtl'
-                    ? <KeyboardArrowLeft />
-                    : <KeyboardArrowRight />
-                }
-            </IconButton>
-            <IconButton
-                aria-label="last page"
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                onClick={handleLastPageButtonClick}
-            >
-                {theme.direction === 'rtl'
-                    ? <FirstPageIcon />
-                    : <LastPageIcon />
-                }
-            </IconButton>
-        </Box>
-    );
-}
-
-TablePaginationActions.propTypes = {
-    count: PropTypes.number.isRequired,
-    onPageChange: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired,
-    rowsPerPage: PropTypes.number.isRequired,
-};
+import { DisplayTable } from './DisplayTable'
 
 function HomePage() {
     const [apiResults, setApiResults] = React.useState([]);
     const [inputValue, setInputValue] = React.useState('');
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
 
     const classes = [
         'Death Knight',
@@ -138,14 +71,6 @@ function HomePage() {
         // {endpoint: 'info',                          title: 'Get Patch Notes'},
     ];
 
-    const tableHeaderLabels = [
-        'Name',
-        'Card Set',
-        'Type',
-        'Class',
-        'Cost',
-    ]
-
     const reachHearthStoneEndpoint = async function (endpoint) {
         console.log('Sending request...')
         const options = {
@@ -169,126 +94,12 @@ function HomePage() {
         }
     }
 
-    const emptyRows = page > 0
-        ? Math.max(0, (1 + page) * rowsPerPage - apiResults.length)
-        : 0;
-
-    const handleChange = (event) => {
-        setInputValue(event.target.value);
-    };
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
-
-    const tableHeader = (
-        <TableHead>
-            <TableRow>
-                {tableHeaderLabels.map(label => {
-                    return (
-                        <TableCell align="center">
-                            {label}
-                        </TableCell>
-                    )
-                })}
-            </TableRow>
-        </TableHead>
-    );
-
-    const tableFooter = (
-        <TableFooter>
-            <TableRow>
-                <TablePagination
-                    colSpan={3}
-                    count={apiResults.length}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                    page={page}
-                    rowsPerPage={rowsPerPage}
-                    rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                    ActionsComponent={TablePaginationActions}
-                    SelectProps={{
-                        inputProps: {
-                            'aria-label': 'rows per page',
-                        },
-                        native: true,
-                    }}
-                />
-            </TableRow>
-        </TableFooter>
-    );
-
-    const emptyTableResults = (
-        <TableRow style={{ height: 53 }}>
-            <TableCell align="center" colSpan={5}  >
-                Nothing to report...
-            </TableCell>
-        </TableRow>
-    );
-    const tableWithResults = (rowsPerPage > 0
-            ? apiResults.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : apiResults
-        ).map((row) => (
-            <TableRow
-                key={row.dbfId}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-                <TableCell
-                    align="center"
-                    component="th"
-                    scope="row"
-                >
-                    {row.name}
-                </TableCell>
-                <TableCell align="center">
-                    {row.cardSet}
-                </TableCell>
-                <TableCell align="center">
-                    {row.type}</TableCell>
-                <TableCell align="center">
-                    {row.playerClass}
-                </TableCell>
-                <TableCell align="center">
-                    {row.cost}
-                </TableCell>
-            </TableRow>
-        ));
-
-    const displayTable = (
-        <TableContainer component={Paper} sx={{maxHeight: 500}}>
-            <Table
-                aria-label="simple table"
-                size="small"
-                sx={{ minWidth: 650 }}
-                stickyHeader
-            >
-                {tableHeader}
-                <TableBody>
-                    {apiResults.length === 0
-                        ? emptyTableResults
-                        : tableWithResults}
-                    {emptyRows > 0 && (
-                        <TableRow style={{ height: 53 * emptyRows }}>
-                            <TableCell colSpan={6} />
-                        </TableRow>
-                    )}
-                </TableBody>
-                {tableFooter}
-            </Table>
-        </TableContainer>
-    );
-
     const requestButtonHtml = lookupValues.map(button => {
         return (
             <Button
                 onClick={e => reachHearthStoneEndpoint(e.target.value)}
                 value={button.endpoint}
-                variant="outlined"
+                variant="contained"
             >
                 {button.title}
             </Button>
@@ -297,7 +108,7 @@ function HomePage() {
 
     const classDropDownMenuItems = classes.map(classs => {
         return (
-            <MenuItem value={classs}>
+            <MenuItem value={classs} >
                 {classs}
             </MenuItem>
         )
@@ -310,7 +121,7 @@ function HomePage() {
                     HearthStone Data Finder
                 </Typography>
                 <Typography variant="h4">
-                    Enter Card Information
+                    Select a class below to get information
                 </Typography>
                 <Select
                     label="Select Class"
@@ -320,7 +131,9 @@ function HomePage() {
                     {classDropDownMenuItems}
                 </Select>
                 {requestButtonHtml}
-                {displayTable}
+                <DisplayTable
+                    results={apiResults}
+                ></DisplayTable>
             </header>
         </div>
     );
