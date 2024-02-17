c413ec4a2726edaa0436d614bb965de7989df259
gFranco <gfranco@velocitymsc.com>
2024-02-17 12:32:58 -0600

    enable table pagination

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,27 +1,101 @@
-import axios from "axios";
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import PropTypes from 'prop-types';
+import axios from "axios";
+
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+
+import {
+    useTheme,
+    Box,
+    Button,
+    IconButton,
+    MenuItem,
+    Paper,
+    Select,
+    SelectChangeEvent,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableFooter,
+    TableHead,
+    TablePagination,
+    TableRow,
+    TextField,
+    Typography,
+} from '@mui/material';
+
 import './HomePage.css';
 
-function HomePage() {
+function TablePaginationActions(props) {
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onPageChange } = props;
 
+    const handleFirstPageButtonClick = (event) => {
+        onPageChange(event, 0);
+    };
+
+    const handleBackButtonClick = (event) => {
+        onPageChange(event, page - 1);
+    };
+
+    const handleNextButtonClick = (event) => {
+        onPageChange(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = (event) => {
+        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+
+    return (
+        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+            <IconButton
+                onClick={handleFirstPageButtonClick}
+                disabled={page === 0}
+                aria-label="first page"
+            >
+                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+            </IconButton>
+            <IconButton
+                onClick={handleBackButtonClick}
+                disabled={page === 0}
+                aria-label="previous page"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            </IconButton>
+            <IconButton
+                onClick={handleNextButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="next page"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </IconButton>
+            <IconButton
+                onClick={handleLastPageButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="last page"
+            >
+                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+            </IconButton>
+        </Box>
+    );
+}
+
+TablePaginationActions.propTypes = {
+    count: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+};
+
+function HomePage() {
     const [inputValue, setInputValue] = React.useState('');
     const [apiResults, setApiResults] = React.useState([]);
-
-    const handleChange = (event) => {
-        setInputValue(event.target.value);
-    };
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
     const classes = [
         'Death Knight',
@@ -52,8 +126,16 @@ function HomePage() {
         // {endpoint: 'info',                          title: 'Get Patch Notes'},
     ];
 
+    const tableHeaderLabels = [
+        'Name',
+        'Card Set',
+        'Type',
+        'Class',
+        'Cost',
+    ]
+
     const reachHearthStoneEndpoint = async function (endpoint) {
-        
+        console.log('Sending request...')
         const options = {
             method: 'GET',
             url: `https://omgvamp-hearthstone-v1.p.rapidapi.com/${endpoint}`,
@@ -64,51 +146,124 @@ function HomePage() {
         };
 
         try {
+            console.log('In Try Block...');
             const response = await axios.request(options);
+            console.log('Setting Requesting...');
             setApiResults(response.data);
         } catch (error) {
             console.error(error);
         }
     }
 
+    const emptyRows = page > 0
+        ? Math.max(0, (1 + page) * rowsPerPage - apiResults.length)
+        : 0;
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
     const displayTable = (
         <TableContainer component={Paper} sx={{maxHeight: 500}}>
-            <Table stickyHeader sx={{ minWidth: 650 }} size="small" aria-label="simple table">
+            <Table
+                aria-label="simple table"
+                size="small"
+                sx={{ minWidth: 650 }}
+                stickyHeader
+            >
                 <TableHead>
                     <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell align="right">Card Set</TableCell>
-                        <TableCell align="right">Type</TableCell>
-                        <TableCell align="right">Class</TableCell>
-                        <TableCell align="right">Cost</TableCell>
+                        {tableHeaderLabels.map(label => {
+                            return (
+                                <TableCell align="center">
+                                    {label}
+                                </TableCell>
+                            )
+                        })}
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {apiResults.map((row) => (
+                    {(rowsPerPage > 0
+                            ? apiResults.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            : apiResults
+                    ).map((row) => (
                         <TableRow
-                            key={row.name}
+                            key={row.dbfId}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                         >
-                            <TableCell component="th" scope="row">
+                            <TableCell align="center" component="th" scope="row">
                                 {row.name}
                             </TableCell>
-                            <TableCell align="right">{row.cardSet}</TableCell>
-                            <TableCell align="right">{row.type}</TableCell>
-                            <TableCell align="right">{row.playerClass}</TableCell>
-                            <TableCell align="right">{row.cost}</TableCell>
+                            <TableCell align="center">
+                                {row.cardSet}
+                            </TableCell>
+                            <TableCell align="center">
+                                {row.type}</TableCell>
+                            <TableCell align="center">
+                                {row.playerClass}
+                            </TableCell>
+                            <TableCell align="center">
+                                {row.cost}
+                            </TableCell>
                         </TableRow>
                     ))}
+                    {emptyRows > 0 && (
+                        <TableRow style={{ height: 53 * emptyRows }}>
+                            <TableCell colSpan={6} />
+                        </TableRow>
+                    )}
                 </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                            ActionsComponent={TablePaginationActions}
+                            colSpan={3}
+                            count={apiResults.length}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                            page={page}
+                            rowsPerPage={rowsPerPage}
+                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                            SelectProps={{
+                                inputProps: {
+                                    'aria-label': 'rows per page',
+                                },
+                                native: true,
+                            }}
+                        />
+                    </TableRow>
+                </TableFooter>
             </Table>
         </TableContainer>
     )
 
     const requestButtonHtml = lookupValues.map(button => {
-        return <Button onClick={e => reachHearthStoneEndpoint(e.target.value)} value={button.endpoint} variant="outlined">{button.title}</Button>
+        return (
+            <Button
+                onClick={e => reachHearthStoneEndpoint(e.target.value)}
+                value={button.endpoint}
+                variant="outlined"
+            >
+                {button.title}
+            </Button>
+        )
     });
 
     const classDropDownMenuItems = classes.map(classs => {
-        return <MenuItem value={classs}>{classs}</MenuItem>
+        return (
+            <MenuItem value={classs}>
+                {classs}
+            </MenuItem>
+        )
     })
 
     return (
